978fbfe448e8c2e8e4188b7a55997ba7079248ab
kik548 <66902553+kik548@users.noreply.github.com>
2021-10-21 08:55:52 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+public  class main{
+public static void main(String[] args) {
+    String hola = "paquito el chocolatero";
+    System.out.println("el  pepe se llama maricarmen");
+System.out.println(longitud(hola));
+}
+
+public static int longitud(String s){
+
+    return s.length();
+}
+
+
+}
